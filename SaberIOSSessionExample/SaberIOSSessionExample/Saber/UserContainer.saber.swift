006fb4b9f6e112995ee6b49abfdad18f076808e6
apleshkov <andrew.pleshkov@gmail.com>
2019-01-07 18:02:24 +0300

    minor update

--- SaberIOSSessionExample/SaberIOSSessionExample/Saber/UserContainer.saber.swift
+++ SaberIOSSessionExample/SaberIOSSessionExample/Saber/UserContainer.saber.swift
@@ -12,22 +12,13 @@ internal class UserContainer: UserContaining {
 
     internal let userExternals: UserExternals
 
-    private var cached_userSessionManagerProvider: UserSessionManagerProvider?
-
     private var cached_userAPI: UserAPI?
+
+    private var cached_userSessionManagerProvider: UserSessionManagerProvider?
 
     internal init(appContainer: AppContainer, userExternals: UserExternals) {
         self.appContainer = appContainer
         self.userExternals = userExternals
-    }
-
-    internal var userSessionManagerProvider: UserSessionManagerProvider {
-        self.lock.lock()
-        defer { self.lock.unlock() }
-        if let cached = self.cached_userSessionManagerProvider { return cached }
-        let userSessionManagerProvider = self.makeUserSessionManagerProvider()
-        self.cached_userSessionManagerProvider = userSessionManagerProvider
-        return userSessionManagerProvider
     }
 
     internal var userAPI: UserAPI {
@@ -39,17 +30,26 @@ internal class UserContainer: UserContaining {
         return userAPI
     }
 
+    internal var userSessionManagerProvider: UserSessionManagerProvider {
+        self.lock.lock()
+        defer { self.lock.unlock() }
+        if let cached = self.cached_userSessionManagerProvider { return cached }
+        let userSessionManagerProvider = self.makeUserSessionManagerProvider()
+        self.cached_userSessionManagerProvider = userSessionManagerProvider
+        return userSessionManagerProvider
+    }
+
     internal var userSessionManager: UserSessionManager {
         let userSessionManager = self.makeUserSessionManager()
         return userSessionManager
     }
 
-    private func makeUserSessionManagerProvider() -> UserSessionManagerProvider {
-        return UserSessionManagerProvider(github: self.appContainer.appExternals.github, accessToken: self.userExternals.accessToken)
-    }
-
     private func makeUserAPI() -> UserAPI {
         return UserAPI(github: self.appContainer.appExternals.github, sessionManager: self.userSessionManager)
+    }
+
+    private func makeUserSessionManagerProvider() -> UserSessionManagerProvider {
+        return UserSessionManagerProvider(github: self.appContainer.appExternals.github, accessToken: self.userExternals.accessToken)
     }
 
     private func makeUserSessionManager() -> UserSessionManager {
